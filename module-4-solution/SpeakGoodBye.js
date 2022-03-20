d78aecab3c2701414270d9ad9674de8377c81a4c
rectangle-man <98609178+rectangle-man@users.noreply.github.com>
2022-03-20 23:33:41 +0530

    Create SpeakGoodBye.js

---
+++ module-4-solution/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function(window){
+
+  var byespeaker={};
+  var speakWord = "Good Bye";
+
+
+  byespeaker.speak=function(name){
+    console.log(speakWord + " " + name);
+  }
+
+  window.byespeaker=byespeaker;
+  
+})(window);
